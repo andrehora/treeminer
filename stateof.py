18e3f302394e2542d0d2926b0dd9bedd4cb26bb1
Andre Hora <andrehoraa@gmail.com>
2025-02-02 20:37:29 -0300

    stateof

--- stateof.py
+++ stateof.py
@@ -1,12 +1,10 @@
 import statistics
 
 from datetime import datetime, date
-from collections import Counter
 from numbers import Number
 
 from tree_sitter import Node
 from treeminer.repo import Repo, Commit
-from extensions import FastAPIMiner
 
 
 def as_str(text: bytes) -> str:
@@ -34,23 +32,33 @@ class NodeList:
     def size(self):
         return len(self.nodes)
     
-    def count_types(self, node_types: list[str]) -> int:
-        return len(self.find_types(node_types))
+    def count_by_type(self, node_types: list[str]) -> int:
+        return len(self.find_by_type(node_types))
     
-    def find_types(self, node_types: list[str]) -> list[Node]:
+    def find_by_type(self, node_types: list[str]) -> list[Node]:
         nodes = []
         for node in self.nodes:
             if node.type in node_types:
                 nodes.append(node)
         return nodes
     
-    def loc_for_types(self, node_types: list[str], stat: str = 'median') -> Number | list[Number]:
-        nodes = self.find_types(node_types)
+    def loc_by_type(self, node_types: list[str], stat: str = 'median') -> Number | list[Number]:
+        nodes = self.find_by_type(node_types)
         locs = [len(as_str(node.text).split('\n')) for node in nodes]
         operation = getattr(statistics, stat)
         return operation(locs)
-    
 
+class MetricHistory:
+
+    def __init__(self, name: str, dates: list[str], values: list[Number]):
+        self.name = name
+        self.dates = dates
+        self.values = values
+    
+    @property
+    def values_as_str(self) -> list[str]:
+        return [str(each) for each in self.values]
+    
 class MetricResult:
 
     def __init__(self, name: str, value: Number, datetime: datetime):
@@ -77,17 +85,18 @@ class ProjectResult:
     def add_commit_result(self, commit_result: CommitResult):
         self.commit_results.append(commit_result)
 
-    def commit_dates(self):
+    def commit_dates(self) -> list[datetime]:
         return [commit_result.datetime.strftime('%m/%Y') for commit_result in self.commit_results]
 
-    def metric_values(self, metric_name: str):
-        return [str(metric_result.value) for metric_result in self._metric_results(metric_name)]
+    def metric_history(self, metric_name: str) -> MetricHistory:
+        dates = self.commit_dates()
+        values = [str(metric_result.value) for metric_result in self._metric_results(metric_name)]
+        return MetricHistory(metric_name, dates, values)
 
-    def metric_values_with_missing_dates(self, metric_name: str, date_unit: str = 'year'):
+    def metric_history_fill_missing_dates(self, metric_name: str, date_unit: str = 'year'):
 
         start_date = self.commit_results[0].datetime
         end_date = self.commit_results[-1].datetime
-        all_dates = self._generate_years(start_date, end_date)
 
         all_dates = []
         if date_unit == 'year':
@@ -100,15 +109,17 @@ class ProjectResult:
         for date in all_dates:
             for metric_result in metric_results:
                 if self._ge(date, metric_result.datetime, date_unit):
-                    values.append(str(metric_result.value))
+                    values.append(metric_result.value)
                     break
-        return self._pretty_dates(all_dates, date_unit), values
+        
+        dates = self._pretty_dates(all_dates, date_unit)
+        return MetricHistory(metric_name, dates, values)
     
     def _pretty_dates(self, dates: list[datetime], date_unit) -> list[str]:
         if date_unit == 'year':
-            return [date.strftime('%Y') for date in dates]
+            return [each.strftime('%Y') for each in dates]
         if date_unit == 'month':
-            return [date.strftime('%m/%Y') for date in dates]
+            return [each.strftime('%m/%Y') for each in dates]
     
     def _ge(self, date1: datetime, date2: datetime, date_unit: str):
         if date_unit == 'year':
@@ -159,22 +170,29 @@ class Result:
     def add_metric_name(self, name):
         self.metric_names.append(name)
 
+    def metric_values_fill_missing_dates(self, date_unit: str = 'year'):
+        for project_result in result.project_results:
+            pass
+            dates, values = project_result.metric_values_fill_missing_dates(metric_name, date_unit)
+            print(dates)
+
 
 class StateOf:
 
-    def __init__(self, name: str, commit_selection: str = 'year'):
+    def __init__(self, name: str, projects: list[str], file_extensions: list[str] | None, commit_selection: str | None = 'year'):
 
         self.name = name
-        self.file_extensions: list[str] = []
-        self.projects: list[str] = []
+        self.projects = projects
+        self.file_extensions = file_extensions
+        self.commit_selection = commit_selection
+
         self.registered_metrics: list[MetricInfo] = []
         self.analyzed_commits: list[str] = []
-        self.commit_selection = commit_selection
 
         only_releases = False
         if commit_selection == 'release':
             only_releases = True
-        self._repo = Repo(projects, only_releases=only_releases)
+        self._repo = Repo(self.projects, only_releases=only_releases)
 
     def metric(self, name: str = None):
         def decorator(func):
@@ -235,79 +253,65 @@ class StateOf:
             _commit_nodes.extend(file_nodes)
         return _commit_nodes
 
-
 # projects = ['git/FastAPI-template']
 # projects = ['git/full-stack-fastapi-template']
-# projects = ['git/FastAPI-template', 'git/full-stack-fastapi-template']
 # projects = ['git/dispatch']
 # projects = ['git/fastapi']
+projects = ['git/FastAPI-template', 'git/full-stack-fastapi-template']
+# projects = ['git/FastAPI-template', 'git/full-stack-fastapi-template', 'git/dispatch', 'git/fastapi']
 
-projects = ['git/FastAPI-template', 'git/full-stack-fastapi-template', 'git/dispatch', 'git/fastapi']
-
-app = StateOf('Foo', commit_selection='year')
-app.projects = projects
-app.file_extensions = ['.py']
+app = StateOf('Foo', projects=projects, file_extensions=['.py'], commit_selection='year')
 
 
-@app.metric(name='all_imports')
-def all_imports(nodes: NodeList):
-    import_nodes = ['import_statement', 'import_from_statement', 'future_import_statement']
-    return nodes.count_types(import_nodes)
+# @app.metric(name='all_imports')
+# def all_imports(nodes: NodeList):
+#     import_nodes = ['import_statement', 'import_from_statement', 'future_import_statement']
+#     return nodes.count_by_type(import_nodes)
 
+# @app.metric(name='import_from_statement')
+# def import_from_statement(nodes: NodeList):
+#     return nodes.count_by_type(['import_from_statement'])
 
-@app.metric(name='import_from_statement')
-def import_from_statement(nodes: NodeList):
-    return nodes.count_types(['import_from_statement'])
+# @app.metric(name='import_statement')
+# def import_statement(nodes: NodeList):
+#     return nodes.count_by_type(['import_statement'])
 
+# @app.metric(name='future_import_statement')
+# def future_import_statement(nodes: NodeList):
+#     return nodes.count_by_type(['future_import_statement'])
 
-@app.metric(name='import_statement')
-def import_statement(nodes: NodeList):
-    return nodes.count_types(['import_statement'])
+# @app.metric(name='classes (LOC)')
+# def classes(nodes: NodeList):
+#     return nodes.loc_by_type(['class_definition'])
 
-
-@app.metric(name='future_import_statement')
-def future_import_statement(nodes: NodeList):
-    return nodes.count_types(['future_import_statement'])
-
+# @app.metric(name='decorated')
+# def decorated(nodes: NodeList):
+#     return nodes.count_by_type(['decorated_definition'])
 
 @app.metric('functions (LOC)')
 def functions(nodes: NodeList):
-    return nodes.loc_for_types(['function_definition'])
-
-
-@app.metric(name='classes (LOC)')
-def classes(nodes: NodeList):
-    return nodes.loc_for_types(['class_definition'])
-
-@app.metric(name='decorated')
-def decorated(nodes: NodeList):
-    return nodes.count_types(['decorated_definition'])
+    return nodes.loc_by_type(['function_definition'])
 
 result = app.compute_metrics()
+# result.metric_values_fill_missing_dates('year')
 
-from rich import print
+# from rich import print
 from rich.console import Console
 from rich.table import Table
 from rich.panel import Panel
-from rich.text import Text
 
 for project_result in result.project_results:
     console = Console()
-    table = Table(show_header=False)
-    print(Panel(project_result.name, expand=False, style='red'))
-    table.add_column("foo", style='green1')
+    table = Table(show_header=True)
+    console.print(Panel(project_result.name, expand=False, style='orange1'))
 
-    table.add_row('dates', *project_result.metric_values_with_missing_dates('functions', 'year')[0])
+    table.add_column('dates', style='green1')
+    dates = project_result.metric_history_fill_missing_dates('functions', 'year').dates
+    for each in dates:
+        table.add_column(each)
 
     for metric_name in result.metric_names:
-
-        # print(project_result.commit_dates())
-        # print(metric_name, project_result.metric_values(metric_name))
-        # table.add_row(metric_name, *project_result.metric_values(metric_name))
-
-        # print(project_result.metric_values_with_missing_dates('functions', 'year')[0])
-        # print(project_result.metric_values_with_missing_dates('functions', 'year')[1])
-
-        table.add_row(metric_name, *project_result.metric_values_with_missing_dates(metric_name, 'year')[1])
+        values = project_result.metric_history_fill_missing_dates(metric_name, 'year').values_as_str
+        table.add_row(metric_name, *values)
 
     console.print(table)
